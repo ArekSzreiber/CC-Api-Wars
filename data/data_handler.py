005ec435b80806bc14abbd982a30e18cd1c6d6f9
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-01-26 23:40:09 +0100

    logging queries

--- data/data_handler.py
+++ data/data_handler.py
@@ -7,3 +7,37 @@ def save_new_user(cursor, username, hashed_password):
         VALUES (%s, %s);
     """, (username, hashed_password)
     )
+
+
+@dc.connection_handler
+def get_user_password(cursor, username):
+    cursor.execute("""
+        SELECT password FROM public.users
+        WHERE username = %(username)s;
+    """, {'username': username,
+          }
+    )
+    result = cursor.fetchall()
+    try:
+        return result[0]['password']
+    except ValueError:
+        return False
+    except IndexError:
+        return False
+
+
+
+
+@dc.connection_handler
+def check_user_existence(cursor, username):
+    cursor.execute("""
+        SELECT username FROM public.users
+        WHERE username = %(username)s;
+    """, {'username': username,
+          }
+    )
+    result = cursor.fetchall()
+    try:
+        return result[0].get('username')
+    except IndexError:
+        return False
