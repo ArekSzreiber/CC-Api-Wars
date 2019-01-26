4015096522d673fde45a08a9ff69b9c2e0f221d5
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-01-26 23:43:45 +0100

    comment corrected

--- validation/validator.py
+++ validation/validator.py
@@ -1,8 +1,9 @@
 import re
+from data import data_handler as data
 
 def is_login_data_valid(username, password):
     if len(password) < 6 or len(password) > 30:
         return False
-    if not re.match(r"^[\w]{3,20}$", username): # if is made of alphanumerical characters
+    if not re.match(r"^[\w]{3,20}$", username): # if is not made of alphanumerical characters
         return False
     return True
