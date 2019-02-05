627acc262cccb4be53ffd7e996681f7fc1c3f657
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-02-05 18:44:11 +0100

    residents table header works

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,10 +1,92 @@
+let keys = ['name',
+            'height',
+            'mass',
+            'hair_color',
+            'skin_color',
+            'eye_color',
+            'birth_year',
+            'gender',
+];
+
+let headers = [ 'Name',
+                'Height',
+                'Mass',
+                'Hair color',
+                'Skin color',
+                'Eye color',
+                'Birth year',
+                'Gender',
+    ];
+
+
+function makeTableHead(headers){
+    let tableHeader = $('#residentsTableHeader');
+    tableHeader.empty();
+    for(let header of headers){
+        let cell = document.createElement('th');
+        cell.textContent = header;
+        tableHeader.append(cell);
+    }
+}
+
+
+function makeTableRow(person){
+    console.log(person);
+    let row = document.createElement('tr');
+    for(let key in keys){
+        let cell = document.createElement('td');
+        cell.textContent = person[key];
+        row.appendChild(cell);
+    }
+    $('#residentsTableBody').append(row);
+
+}
+
+function makeTableBody(residentsURLs){
+    $('#residentsTableBody').empty();
+    for(let residentURL of residentsURLs){
+        let person = new XMLHttpRequest();
+        person.onreadystatechange = function(){
+            if(person.readyState === 4){
+                makeTableRow(JSON.parse(person.response))
+            }
+        };
+        person.open("GET", residentURL, true);
+        person.send();
+    }
+
+
+}
+
+
+function makeTable(headers, residents){
+
+    makeTableHead(headers);
+    makeTableBody(residents);
+}
+
+
+
+
 $('#residentsModal').on('show.bs.modal', function (event) {
-  let button = $(event.relatedTarget); // Button that triggered the modal
-  let planetName = button.data('planetName'); // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  let modal = $(this);
-  console.log(button);
-  modal.find('.modal-title').text('Residents of ' + planetName);
-  modal.find('.modal-body input').val(planetName);
+    let button = $(event.relatedTarget); // Button that triggered the modal
+    let planetUrl = button.data('planetUrl'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    let modal = $(this);
+    modal.find('.modal-title').text('Residents of ' + button.data('planetName'));
+    modal.find('.modal-body input').val(planetUrl);
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            let planet = JSON.parse(xhr.response);
+            makeTable(headers, planet['residents']);
+            $("#residentsTable").innerHTML = planet;
+        }
+    };
+
+    xhr.open("GET", planetUrl, true);
+    xhr.send();
 });
+
