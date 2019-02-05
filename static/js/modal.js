d48476927b0523b7915a1c4047783be8fd31a2d0
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-02-05 12:29:10 +0100

    residentsModal header done

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,9 +1,10 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
+$('#residentsModal').on('show.bs.modal', function (event) {
   let button = $(event.relatedTarget); // Button that triggered the modal
-  let recipient = button.data('whatever'); // Extract info from data-* attributes
+  let planetName = button.data('planetName'); // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   let modal = $(this);
-  modal.find('.modal-title').text('New message to ' + recipient);
-  modal.find('.modal-body input').val(recipient);
+  console.log(button);
+  modal.find('.modal-title').text('Residents of ' + planetName);
+  modal.find('.modal-body input').val(planetName);
 });
